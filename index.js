f931962310e1c6e4ad019781a537f7c09feb305a
ameeralih <ameerali.hassanali@gmail.com>
2023-02-28 19:03:21 +0800

    user edit

--- index.js
+++ index.js
@@ -10,6 +10,26 @@ const db = require("./src/db/models/index.js");
 const { user, review } = db;
 const { Op } = require("sequelize");
 
+app.patch("/users/:userId", async (req, res, next) => {
+  const userId = req.params.userId;
+  const { firstName, lastName, company, email } = req.body;
+  try {
+    const thisUser = await user.findOne({ where: { id: userId } });
+    if (!thisUser) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    thisUser.firstName = firstName || thisUser.firstName;
+    thisUser.lastName = lastName || thisUser.lastName;
+    thisUser.company = company || thisUser.company;
+    thisUser.email = email || thisUser.email;
+    await thisUser.save();
+    return res.status(200).json({ message: "User updated successfully" });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+});
+
 app.get("/users", async (req, res) => {
   const users = await user.findAll();
   res.json(users);
